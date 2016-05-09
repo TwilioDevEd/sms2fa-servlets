c0a94d54decebf419674aaba474491e66c0a1ade
Marcos Oliveira Sampaio <mosampaio@gmail.com>
2016-05-09 18:13:38 -0300

    Adds index.jsp

--- src/main/java/com/twilio/sms2fa/application/servlets/Sms2faServletModule.java
+++ src/main/java/com/twilio/sms2fa/application/servlets/Sms2faServletModule.java
@@ -1,10 +1,14 @@
 package com.twilio.sms2fa.application.servlets;
 
 import com.google.inject.servlet.ServletModule;
+import com.twilio.sms2fa.domain.repository.UserRepository;
+import com.twilio.sms2fa.infrastructure.repository.UserInMemoryRepository;
 
 public class Sms2faServletModule extends ServletModule {
     @Override
     public void configureServlets() {
         serve("/users/").with(UserServlet.class);
+
+        bind(UserRepository.class).to(UserInMemoryRepository.class);
     }
 }
